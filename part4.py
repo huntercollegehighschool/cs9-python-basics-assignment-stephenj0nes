52298cb9399980f6e6f34634954600468329f8f5
Stephen Jones <>
2022-05-01 18:58:08 +0000

    finished

--- part4.py
+++ part4.py
@@ -12,5 +12,10 @@ Enter the height: 3
 The area of the triangle is 12.0
 
 '''
-
 #start writing your code below
+
+base = int(input("Enter the base: "))
+height = int(input("Enter the height: "))
+area = (base * height) / 2
+
+print("The area of the triangle is", area)
